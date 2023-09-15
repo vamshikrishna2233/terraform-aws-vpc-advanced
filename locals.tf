273cdd576fc62473fdd2573500f2184dadca202b
NandiVamshiKrishna <nandivamshikrishna@gmail.com>
2023-09-15 13:47:03 -0400

    vpc module

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  azs = slice(data.aws_availability_zones.available.names,0,2)
+}
+
---
+++ variables.tf
@@ -0,0 +1,74 @@
+# forcing user to provide value
+variable "cidr_block" {
+  
+}
+
+# optional, because we gave default value
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "project_name" {
+  
+}
+
+# even optional, it is good to give tags
+variable "common_tags" {
+  default = {}
+}
+
+variable "vpc_tags" {
+  default = {}
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidr" {
+  type = list
+  validation {
+    condition     = length(var.public_subnet_cidr) == 2
+    error_message = "Please provide 2 public subnet CIDR"
+  }
+}
+
+variable "private_subnet_cidr" {
+  type = list
+  validation {
+    condition     = length(var.private_subnet_cidr) == 2
+    error_message = "Please provide 2 private subnet CIDR"
+  }
+}
+
+variable "database_subnet_cidr" {
+  type = list
+  validation {
+    condition     = length(var.database_subnet_cidr) == 2
+    error_message = "Please provide 2 database subnet CIDR"
+  }
+}
+
+variable "nat_gateway_tags" {
+  default = {}
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "db_subnet_group_tags" {
+  default = {}
+}
